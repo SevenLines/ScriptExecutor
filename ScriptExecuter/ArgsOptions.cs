bec270e4e042e23f3720867b47920212f7ef92a3
SevenLines <mmailm@mail.ru>
2013-11-29 02:34:50 +0900

    init

---
+++ ScriptExecuter/ArgsOptions.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    #region Using Directives
+    using CommandLine;
+    using CommandLine.Text;
+    #endregion
+
+    internal class Options
+    {
+        [Option('c', "command", Required = true, HelpText = "sql script or path to file with sql script")]
+        public string Command { get; set; }
+
+        [Option('l', "delimiter", HelpText = "columns delimeter", DefaultValue = "\t")]
+        public string Delimiter { get; set; }
+
+        [Option('s', "source", HelpText = "DataBase server, e.g. (local)", DefaultValue = "(local)")]
+        public string Source { get; set; }
+
+        [Option('d', "database", Required = true, HelpText = "DataBase name")]
+        public string Database { get; set; }
+
+        [Option('u', "user", HelpText = "user", DefaultValue = "")]
+        public string User { get; set; }
+
+        [Option('p', "password", HelpText = "password", DefaultValue="")]
+        public string Password { get; set; }
+
+        [Option('v', "verbose", HelpText = "verbose")]
+        public bool Verbose { get; set; }
+
+        [Option('o', "output", HelpText = "output file path")]
+        public string OutputFilePath { get; set; }
+
+        [Option('e', "encoding", HelpText = "Code page, see Encoding .Net class doc for the list of available values", DefaultValue=1251)]
+        public int Encoding { get; set; }
+
+        [HelpOption]
+        public string GetUsage()
+        {
+            return HelpText.AutoBuild(this, current => HelpText.DefaultParsingErrorsHandler(this, current));
+        }
+    }
+}
---
+++ ScriptExecuter/Program.cs
@@ -0,0 +1,131 @@
+﻿#region Using Directives
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+using System.Data.SqlClient;
+
+using CommandLine;
+using CommandLine.Text;
+#endregion
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static IEnumerable<string> Fields(SqlDataReader reader)
+        {
+            for (int i = 0; i < reader.FieldCount; ++i)
+            {
+                yield return reader[i].ToString();
+            }
+        }
+        
+        static void Main(string[] args)
+        {
+            var options = new Options();
+            StreamWriter sw = null;
+            SqlDataReader reader = null;
+            
+            // parse ommand line arguments
+            if (!CommandLine.Parser.Default.ParseArguments(args, options))
+            {
+                Environment.Exit(CommandLine.Parser.DefaultExitCodeFail);
+            }
+
+            // reading script text
+            String script;
+            if (File.Exists(options.Command)) // trying to open file first
+            {
+                script = File.ReadAllText(options.Command);
+            } else { // on fail just use command as script text
+                script = options.Command;
+            }
+
+            // build connection string
+            var sb = new SqlConnectionStringBuilder();
+            if (options.Password!="") sb.Password = options.Password;
+            if (options.User != "") sb.UserID = options.User;
+            sb.DataSource = options.Source;
+            sb.InitialCatalog = options.Database;
+
+            // connection to database
+            if (options.Verbose) Console.WriteLine("# Trying to connect to DataBase");
+            SqlConnection connection = new SqlConnection(sb.ToString());
+            try
+            {
+                connection.Open();
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine(e.Message);
+                return; // exit on fail
+            }
+
+            // creating command script
+            SqlCommand command = new SqlCommand();
+            command.Connection = connection;
+            command.CommandText = script;
+
+            // trying to run command
+            if (options.Verbose) Console.WriteLine("# Trying to run command");
+            try
+            {
+                reader = command.ExecuteReader();
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine(e.Message);
+                return; // exit on fail
+            }
+
+            // trying to open output stream if any
+            try
+            {
+                if (options.OutputFilePath != null)
+                {
+                    sw = new StreamWriter(options.OutputFilePath, false,
+                        Encoding.GetEncoding(options.Encoding));
+                }
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine(e.Message);
+                return; // exit on fail
+            }
+
+            // trying to itterate ove result
+            try
+            {
+                if (options.Verbose) Console.WriteLine("# Itterate over result");
+                while (reader.Read())
+                {
+                    String row = String.Join(options.Delimiter, Fields(reader));
+                    // write to file either to console
+                    if (sw!=null) sw.WriteLine(row);
+                    else Console.WriteLine(row);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine(e.Message);
+            }
+            finally
+            {
+                // cleaning
+                if (sw != null) sw.Close();
+
+                if (options.Verbose) Console.WriteLine("# Closing reader");
+                reader.Close();
+            }
+
+            // closing connection
+            if (options.Verbose) Console.WriteLine("# Closing connection");
+            connection.Close();
+            
+            if (options.Verbose) Console.WriteLine("# Done");
+        }
+    }
+}
